a8f50c9b5e327ec44ba4ea790bfed0c1e7586b7d
chunbikm59 <chunbikm59@gmail.com>
2021-07-18 20:15:54 +0800

    Update BluetoothTerminal.js

--- Mini-Bot/js/BluetoothTerminal.js
+++ Mini-Bot/js/BluetoothTerminal.js
@@ -243,7 +243,7 @@ class BluetoothTerminal {
 
     return navigator.bluetooth.requestDevice({
       //filters: [{services: [this._serviceUuid]}],
-	  // filters: [{name: 'Mini-Bot'}],
+	  filters: [],
 	  optionalServices: [
 	  '0000ffe0-0000-1000-8000-00805f9b34fb',
 	  '00001801-0000-1000-8000-00805f9b34fb',
